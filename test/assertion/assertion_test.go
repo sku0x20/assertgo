f9e3528ef994726b91e7531c5d86e678b1812e24
siddhant <siddhantkumarupmanyu@gmail.com>
2026-06-12 04:38:12 +0530

    Remove unused sink import and simplify newAssertion usage in assertion_test.go

--- test/assertion/assertion_test.go
+++ test/assertion/assertion_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/sku0x20/assertgo/pkg/assertion"
 	"github.com/sku0x20/assertgo/pkg/matcherasserter"
-	"github.com/sku0x20/assertgo/pkg/sink"
 	agtest "github.com/sku0x20/assertgo/test"
 )
 
@@ -26,10 +25,6 @@ func Test_AnyAssertion_Not(t *testing.T) {
 	assert(t, "hello", true, func(a *assertion.Assertion[any]) { a.Not().EqualTo("hello") })
 }
 
-func newAssertion(s *sink.TSink, value any) *assertion.Assertion[any] {
-	return assertion.New(matcherasserter.New(s, value))
-}
-
 func assertPasses(t *testing.T, value any, fn func(*assertion.Assertion[any])) {
 	assert(t, value, false, fn)
 }
@@ -37,7 +32,8 @@ func assertPasses(t *testing.T, value any, fn func(*assertion.Assertion[any])) {
 func assert(t *testing.T, value any, fail bool, fn func(*assertion.Assertion[any])) {
 	t.Helper()
 	mock, s := agtest.NewSink()
-	fn(newAssertion(s, value))
+	a := assertion.New(matcherasserter.New(s, value))
+	fn(a)
 	if mock.FatalCalled != fail {
 		t.Fatal("expected failure")
 	}
